e2e0728528e9c2ccb7e0382188dd0bedb3e515d0
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2025-09-07 21:19:56 +0530

    BN-171 | Add. Controls Helper RTL Test

---
+++ test/helpers/controlsHelper.test.js
@@ -0,0 +1,50 @@
+import { getValidations } from 'src/helpers/controlsHelper';
+import constants from 'src/constants';
+
+describe('ControlsHelper', () => {
+  describe('should test getValidations', () => {
+    it('get mandatory, allowDecimal validations from properties', () => {
+      const props = { mandatory: true };
+      const conceptProperties = { allowDecimal: false };
+      const validations = getValidations(props, conceptProperties);
+
+      expect(validations.length).toBe(2);
+      expect(validations[0]).toBe(constants.validations.mandatory);
+      expect(validations[1]).toBe(constants.validations.allowDecimal);
+    });
+
+    it('get mandatory validations from properties', () => {
+      const props = { mandatory: true };
+      const validations = getValidations(props, undefined);
+
+      expect(validations.length).toBe(1);
+      expect(validations[0]).toBe(constants.validations.mandatory);
+    });
+
+    it('get allowFutureDates validations from properties', () => {
+      const props = { allowFutureDates: false };
+      const validations = getValidations(props, undefined);
+
+      expect(validations.length).toBe(1);
+      expect(validations[0]).toBe(constants.validations.allowFutureDates);
+    });
+
+    it('get allowDecimal validations from properties', () => {
+      let conceptProperties = { allowDecimal: false };
+      let validations = getValidations({}, conceptProperties);
+
+      expect(validations.length).toBe(1);
+      expect(validations[0]).toBe(constants.validations.allowDecimal);
+
+      conceptProperties = { allowDecimal: true };
+      validations = getValidations({}, conceptProperties);
+      expect(validations.length).toBe(0);
+    });
+
+    it('should not throw exceptions for properties without validations', () => {
+      const validations = getValidations(undefined, undefined);
+
+      expect(validations.length).toBe(0);
+    });
+  });
+});
