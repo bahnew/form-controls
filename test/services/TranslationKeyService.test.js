514dd0af76e879e39db95bddbc2a936333e5a2b2
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2025-09-07 18:57:06 +0530

    BN-171 | Add. Translation Service RTL Test

---
+++ test/services/TranslationKeyService.test.js
@@ -0,0 +1,23 @@
+import TranslationKeyGenerator from 'src/services/TranslationKeyService';
+
+describe('TranslationKeyService', () => {
+  it('should generate a translation key', () => {
+    const transKeyGenerator = new TranslationKeyGenerator('label', '1');
+    expect(transKeyGenerator.build()).toBe('LABEL_1');
+  });
+
+  it('should generate a translation key by replacing spaces', () => {
+    const transKeyGenerator = new TranslationKeyGenerator('some dummy label', '1');
+    expect(transKeyGenerator.build()).toBe('SOME_DUMMY_LABEL_1');
+  });
+
+  it('should handle empty string input', () => {
+    const transKeyGenerator = new TranslationKeyGenerator('', '1');
+    expect(transKeyGenerator.build()).toBe('_1');
+  });
+
+  it('should use default id of 0 when not provided', () => {
+    const generator = new TranslationKeyGenerator('label');
+    expect(generator.build()).toBe('LABEL_0');
+  });
+});
